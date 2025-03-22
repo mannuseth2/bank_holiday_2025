f5a76003e30426587bf8dc226e8d874a8ee6fe95
IphoneWala <rockume@gmail.com>
2025-03-22 20:10:04 +0530

    Initial Commit

---
+++ BankWala/BankWalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankWalaApp.swift
+//  BankWala
+//
+//  Created by Mannu Seth on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BankWalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
